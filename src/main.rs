e811bd0d3ed187953ba3d4f7f53be6985a042ea8
Andrew Langmeier <raymi306@gmail.com>
2025-03-25 09:48:30 -0400

    remove instructions from LICENSE, place notice in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+//
+//  A memorable password generator inspired by xkcd comic 936.
+//  Copyright (C) 2025  Andrew Langmeier
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published
+//  by the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/agpl-3.0.txt>.
+//
 //! Create memorable passwords.
 //!
 //! Use custom configurations, or roll with the defaults.
